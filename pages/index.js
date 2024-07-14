073b48be7139e0b5beb92157ceadbefd23fb81f2
BSL Software <bruno.lima@bslsoftware.com.br>
2024-07-14 13:46:37 +0000

    testando

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import Head from "next/head";
-import styles from "../styles/Construcao.module.css";
-import Image from 'next/image';
-import logo from "../assets/logocofcofpng.png"
+// import Head from "next/head";
+// import styles from "../styles/Construcao.module.css";
+// import Image from 'next/image';
+// import logo from "../assets/logocofcofpng.png"
 
 function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
+    // <div className={styles.container}>
+    <div>
+      {/* <Head>
         <title>Em Construção</title>
       </Head>
-      <main className={styles.main}>         
-        <Image src={logo} alt="Logo da Smoke Shop" className={styles.logo} />
+      <main className={styles.main}>          */}
+      <main>
+        {/* <Image src={logo} alt="Logo da Smoke Shop" className={styles.logo} /> */}
         <h1>Em breve a maior smoke shop do Brasil!!</h1>       
       </main>
     </div>
