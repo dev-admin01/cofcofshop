553297cf9e48e882e45fe2d2cdea688beba93ef3
BSL Software <bruno.lima@bslsoftware.com.br>
2024-07-14 14:27:59 +0000

    fix: comment removal

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
-// import Head from "next/head";
-// import styles from "../styles/Construcao.module.css";
-// import Image from 'next/image';
-// import logo from "../assets/logocofcofpng.png"
+import Head from "next/head";
+import styles from "../styles/Construcao.module.css";
+import Image from 'next/image';
+import logo from "../assets/logocofcofpng.png"
 
 function Home() {
   return (
-    // <div className={styles.container}>
-    <div>
-      {/* <Head>
+    <div className={styles.container}>
+      <Head>
         <title>Em Construção</title>
       </Head>
-      <main className={styles.main}>          */}
-      <main>
-        {/* <Image src={logo} alt="Logo da Smoke Shop" className={styles.logo} /> */}
-        <h1>Em breve a maior smoke shop do Brasil!!</h1>       
+      <main className={styles.main}>
+        <Image src={logo} alt="Logo da Smoke Shop" className={styles.logo} /> */}
+        <h1>Em breve a maior smoke shop do Brasil!!</h1>
       </main>
     </div>
   );
