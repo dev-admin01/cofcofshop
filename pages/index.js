bdd1b2b1c34c91a50f1bd5edf336cdfe02635833
BSL Software <bruno.lima@bslsoftware.com.br>
2024-07-14 13:11:53 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import styles from "../styles/Construcao.module.css";
+import Image from 'next/image';
+import logo from "../assets/logocofcofpng.png"
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Em Construção</title>
+      </Head>
+      <main className={styles.main}>         
+        <Image src={logo} alt="Logo da Smoke Shop" className={styles.logo} />
+        <h1>Em breve a maior smoke shop do Brasil!!</h1>       
+      </main>
+    </div>
+  );
+}
+
+export default Home;
