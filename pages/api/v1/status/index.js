e105c804892d3a15a8649c6d4bfc2d28ec81e348
bruno-lima1504 <bruno.lima1504@gmail.com>
2024-08-18 19:09:22 -0300

    add:

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,37 @@
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const updatedAt = new Date().toDateString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections"
+  );
+  const dataBaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
+  });
+}
+
+export default status;
